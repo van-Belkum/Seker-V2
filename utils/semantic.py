6e38bbb9a974daf9c8f9ef1fb076fc5a7b7b3e2b
van-Belkum <vanbelkumd@yahoo.com>
2025-09-01 16:17:53 +0100

    Delete utils directory

--- utils/semantic.py
+++
@@ -1,7 +0,0 @@
-from rapidfuzz import fuzz, process
-
-def fuzzy_contains(haystack: str, needle: str, threshold: int = 80) -> bool:
-    hay = haystack.lower()
-    ned = needle.lower()
-    score = fuzz.partial_ratio(hay, ned)
-    return score >= threshold
